34d9b87e897c08d1eeb8598321db70b09d0ebbe5
nskiu <nyelsku@gmail.com>
2023-06-12 08:19:52 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Display from "./components/display";
+import Buttons from "./components/buttons";
+
+const App = () => {
+  return (
+    <div id="calculator">
+      <Display />
+      <Buttons />
+    </div>
+  );
+};
+
+export default App;
